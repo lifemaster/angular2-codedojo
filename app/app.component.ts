98cc5bcc090fdfb8471babf13cb6ee00a5e7392f
lifemasterJS <konstantin.shyika@workrocks.ua>
2017-08-02 23:15:43 +0300

    Usage directives ngFor, ngIf, ngClass for repeat todos

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,20 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  {
+    title: 'Изучить JavaScript',
+    completed: true
+  },
+  {
+    title: 'Изучить Angular 2',
+    completed: false
+  },
+  {
+    title: 'Написать приложение',
+    completed: false
+  }
+]
+
 @Component({
   moduleId: module.id,
   selector: 'app',
@@ -9,4 +24,5 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Angular Todo';
+  todos = todos;
 }
