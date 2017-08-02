ba8cfbb545eb6aa78341a9302e234868ae6741bd
lifemasterJS <konstantin.shyika@workrocks.ua>
2017-08-02 22:26:31 +0300

    Refactored main component
    1. Created html and css files for main component;

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app',
-  template: '<h1>Angular Todo</h1>'
+  templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.css' ]
 })
 
 export class AppComponent {
-
+  title = 'Angular Todo';
 }
