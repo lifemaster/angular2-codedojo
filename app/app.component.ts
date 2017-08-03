c629a62abe8688b4eabc32c1ee9ff5d5d0aedc81
lifemaster <kostya.shyika@gmail.com>
2017-08-03 14:17:40 +0300

    Decomposition
    1. Created components: todo-list and todo-item;
    2. Created folder "shared" with 'data.ts' and 'todo.ts';

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,5 @@
 import { Component } from '@angular/core';
 
-class Todo {
-  constructor(public title: string, public completed: boolean = false) {};
-}
-
-const todos: Todo[] = [
-  {
-    title: 'Изучить JavaScript',
-    completed: true
-  },
-  {
-    title: 'Изучить Angular 2',
-    completed: false
-  },
-  {
-    title: 'Написать приложение',
-    completed: false
-  }
-]
-
 @Component({
   moduleId: module.id,
   selector: 'app',
@@ -28,17 +9,4 @@ const todos: Todo[] = [
 
 export class AppComponent {
   title = 'Angular Todo';
-  todos: Todo[] = todos;
-
-  toggle(todo: Todo) {
-    todo.completed = !todo.completed;
-  }
-
-  delete(todo: Todo) {
-    let index = todos.indexOf(todo);
-
-    if(index > -1) {
-      todos.splice(index, 1);
-    }
-  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
   declarations: [
     AppComponent,
-    TodoFormComponent
+    TodoFormComponent,
+    TodoListComponent,
+    TodoItemComponent
    ],
   bootstrap: [ AppComponent ]
 })
